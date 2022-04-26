86feae5d254839b2ee3cc798f7bcfad5ae2a37ce
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-04-26 10:54:30 +0300

    Link sing up page elements to SignUpVC and refactor text fields

---
+++ CheckRooMate/SignUpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewController.swift
+//  CheckRooMate
+//
+//  Created by Furkan Açıkgöz on 26.04.2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldSurname: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func buttonSignUpTapped(_ sender: Any) {
+        
+    }
+    
+}

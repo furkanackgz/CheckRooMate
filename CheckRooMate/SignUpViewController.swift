2edf27cf5c803bf2944cfca2338c9f2b99606bde
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-04-26 11:01:19 +0300

    Add hide keyboard functionality to SignUpVC

--- CheckRooMate/SignUpViewController.swift
+++ CheckRooMate/SignUpViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textFieldUsername: UITextField!
     @IBOutlet weak var textFieldPassword: UITextField!
@@ -18,7 +18,27 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        textFieldUsername.delegate = self
+        textFieldPassword.delegate = self
+        textFieldName.delegate = self
+        textFieldSurname.delegate = self
+        textFieldEmail.delegate = self
         
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.textFieldUsername.resignFirstResponder()
+        self.textFieldPassword.resignFirstResponder()
+        self.textFieldName.resignFirstResponder()
+        self.textFieldSurname.resignFirstResponder()
+        self.textFieldEmail.resignFirstResponder()
+        return true
     }
     
     @IBAction func buttonSignUpTapped(_ sender: Any) {

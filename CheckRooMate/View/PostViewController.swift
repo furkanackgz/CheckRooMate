6a82d614ffe21e8b5d577a6bb0c4f75314bf1871
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-05 14:54:42 +0300

    Add done Bar Button in PostVC's NavBar

--- CheckRooMate/View/PostViewController.swift
+++ CheckRooMate/View/PostViewController.swift
@@ -11,8 +11,16 @@ class PostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .done, target: self, action: #selector(doneBttnTapped))
         
     }
-
+    
+    @objc func doneBttnTapped() {
+        navigationController?.popViewController(animated: true)
+    }
 }

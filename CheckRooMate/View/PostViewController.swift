fdbe203aa69cfcef30c433977516293dedfca1f8
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-05 18:05:31 +0300

    Add textView and add doneBttn to Keyboard

--- CheckRooMate/View/PostViewController.swift
+++ CheckRooMate/View/PostViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textView.text = "Enter text"
+        textView.textColor = UIColor.lightGray
+        
+        textView.delegate = self
+        
+        addDoneBttnToKeyboard()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,5 +31,36 @@ class PostViewController: UIViewController {
     
     @objc func doneBttnTapped() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+                textView.text = nil
+                textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Enter text"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+    func addDoneBttnToKeyboard() {
+        
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBttn = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(hideKeyboard))
+        
+        toolBar.setItems([flexibleSpace,doneBttn], animated: true)
+        
+        self.textView.inputAccessoryView = toolBar
+    }
+    
+    @objc func hideKeyboard(){
+        self.view.endEditing(true)
     }
 }

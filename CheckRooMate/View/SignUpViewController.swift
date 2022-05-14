5497923ad3c6311be5fbaed24f4f2c09d81da559
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-14 22:59:16 +0300

    Rearrange SignUpVC and add alert functionailty for fields

--- CheckRooMate/View/SignUpViewController.swift
+++ CheckRooMate/View/SignUpViewController.swift
@@ -7,45 +7,89 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController, UITextFieldDelegate {
+class SignUpViewController: UIViewController{
 
-    @IBOutlet weak var textFieldUsername: UITextField!
-    @IBOutlet weak var textFieldPassword: UITextField!
-    @IBOutlet weak var textFieldPasswordAgain: UITextField!
-    @IBOutlet weak var textFieldName: UITextField!
-    @IBOutlet weak var textFieldSurname: UITextField!
-    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldUsername: UITextField! {
+        didSet{
+            textFieldUsername.delegate = self
+        }
+    }
+    @IBOutlet weak var textFieldPassword: UITextField! {
+        didSet{
+            textFieldPassword.delegate = self
+        }
+    }
+    @IBOutlet weak var textFieldPasswordAgain: UITextField! {
+        didSet{
+            textFieldPasswordAgain.delegate = self
+        }
+    }
+    @IBOutlet weak var textFieldName: UITextField! {
+        didSet{
+            textFieldName.delegate = self
+        }
+    }
+    @IBOutlet weak var textFieldSurname: UITextField! {
+        didSet{
+            textFieldSurname.delegate = self
+        }
+    }
+    @IBOutlet weak var textFieldEmail: UITextField! {
+        didSet{
+            textFieldEmail.delegate = self
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textFieldUsername.delegate = self
-        textFieldPassword.delegate = self
-        textFieldPasswordAgain.delegate = self
-        textFieldName.delegate = self
-        textFieldSurname.delegate = self
-        textFieldEmail.delegate = self
-        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
     }
+    
+    @IBAction func buttonSignUpTapped(_ sender: Any) {
+        
+        guard let username = textFieldUsername.text, let password = textFieldPassword.text,
+              let passwordAgain = textFieldPasswordAgain.text, let name = textFieldName.text,
+              let surname = textFieldSurname.text, let email = textFieldEmail.text else {
+                return
+        }
+        
+        if username.isEmpty || password.isEmpty || passwordAgain.isEmpty ||
+           name.isEmpty || surname.isEmpty || email.isEmpty {
+            displayAlertMessage("You need to fill all the fields!")
+            return
+        }
+        
+        if password != passwordAgain {
+            displayAlertMessage("Passwords are not matched!")
+            return
+        }
+    }
+    
+}
+
+extension SignUpViewController: UITextFieldDelegate {
     
     @objc func hideKeyboard() {
         view.endEditing(true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.textFieldUsername.resignFirstResponder()
-        self.textFieldPassword.resignFirstResponder()
-        self.textFieldPasswordAgain.resignFirstResponder()
-        self.textFieldName.resignFirstResponder()
-        self.textFieldSurname.resignFirstResponder()
-        self.textFieldEmail.resignFirstResponder()
+        textFieldUsername.resignFirstResponder()
+        textFieldPassword.resignFirstResponder()
+        textFieldPasswordAgain.resignFirstResponder()
+        textFieldName.resignFirstResponder()
+        textFieldSurname.resignFirstResponder()
+        textFieldEmail.resignFirstResponder()
         return true
     }
     
-    @IBAction func buttonSignUpTapped(_ sender: Any) {
+    func displayAlertMessage(_ message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        let alertButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(alertButton)
         
+        self.present(alert, animated: true, completion: nil)
     }
-    
 }

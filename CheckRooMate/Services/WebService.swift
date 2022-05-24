67514ef5c5446a7ea0078adcd82341a418fba9fd
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-24 15:51:33 +0300

    Refactor WebService by classifying functions into different extensions

--- CheckRooMate/Services/WebService.swift
+++ CheckRooMate/Services/WebService.swift
@@ -12,6 +12,61 @@ class WebService {
     static let run: WebService = WebService()
     let session = URLSession.shared
     
+}
+
+// Sign In User
+extension WebService {
+    
+    func signInUser(_ username: String, _ password: String, _ CompletionHandler: @escaping (IsLoginSuccessfulResponse?)->(Void)) {
+        
+        guard let url = URL(string: "https://bezkoder-server.herokuapp.com/api/login") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let json = [
+            "username" : username,
+            "password" : password
+        ]
+        
+        do{
+            request.httpBody = try JSONSerialization.data(withJSONObject: json)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        let _ = session.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+               print("Server error!")
+               return
+            }
+            
+            if let data = data {
+                let json = try? JSONDecoder().decode(IsLoginSuccessfulResponse.self, from: data)
+                
+                if let json = json {
+                    CompletionHandler(json)
+                }
+            }
+            
+        }.resume()
+        
+    }
+    
+}
+
+// Sign Up User
+extension WebService {
+    
     func signUpUser(_ user: UserRegistrationRequest, _ CompletionHandler: @escaping (UserRegistrationResponse?)->(Void)) {
         
         guard let url = URL(string: "https://bezkoder-server.herokuapp.com/api/insertUser") else {
@@ -58,48 +113,4 @@ class WebService {
         task.resume()
     }
     
-    func signInUser(_ username: String, _ password: String, _ CompletionHandler: @escaping (IsLoginSuccessfulResponse?)->(Void)) {
-        
-        guard let url = URL(string: "https://bezkoder-server.herokuapp.com/api/login") else {
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let json = [
-            "username" : username,
-            "password" : password
-        ]
-        
-        do{
-            request.httpBody = try JSONSerialization.data(withJSONObject: json)
-        } catch let error {
-            print(error.localizedDescription)
-        }
-        
-        let _ = session.dataTask(with: request) { data, response, error in
-            
-            if let error = error {
-                print(error)
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-               print("Server error!")
-               return
-            }
-            
-            if let data = data {
-                let json = try? JSONDecoder().decode(IsLoginSuccessfulResponse.self, from: data)
-                
-                if let json = json {
-                    CompletionHandler(json)
-                }
-            }
-            
-        }.resume()
-        
-    }
 }

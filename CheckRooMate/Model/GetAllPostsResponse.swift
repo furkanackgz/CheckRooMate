847e6cbf4b6355a9b1f7a6caa77f97e4844b836e
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-24 16:35:57 +0300

    Add Post and GetAllPostsResponse models

---
+++ CheckRooMate/Model/GetAllPostsResponse.swift
@@ -0,0 +1,12 @@
+//
+//  GetAllPostsResponse.swift
+//  CheckRooMate
+//
+//  Created by Furkan Açıkgöz on 24.05.2022.
+//
+
+import Foundation
+
+struct GetAllPostsResponse {
+    var posts: [Post]?
+}
---
+++ CheckRooMate/Model/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  CheckRooMate
+//
+//  Created by Furkan Açıkgöz on 24.05.2022.
+//
+
+import Foundation
+
+struct Post {
+    
+    var postId: Int?
+    var userId: Int?
+    var postContent: String?
+    var images: String?
+    var postTime: String?
+    
+}

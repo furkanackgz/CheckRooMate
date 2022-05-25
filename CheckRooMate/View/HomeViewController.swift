a0d70a09a02c485b192ca655f9909e094d29e1be
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-25 09:44:20 +0300

    Reorganize HomeVC by adding extensions

--- CheckRooMate/View/HomeViewController.swift
+++ CheckRooMate/View/HomeViewController.swift
@@ -7,37 +7,65 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+class HomeViewController: UIViewController {
     
-    @IBOutlet weak var labelAreYouLookingFor: UILabel!
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var labelAreYouLookingFor: UILabel! {
+        didSet{
+            labelAreYouLookingFor.isUserInteractionEnabled = true
+            let tap = UITapGestureRecognizer(target: self, action: #selector(toPostVC))
+            labelAreYouLookingFor.addGestureRecognizer(tap)
+        }
+    }
+    @IBOutlet weak var tableView: UITableView! {
+        didSet{
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        labelAreYouLookingFor.isUserInteractionEnabled = true
-        let tap = UITapGestureRecognizer(target: self, action: #selector(toPostVC))
-        //tap.cancelsTouchesInView = false
-        labelAreYouLookingFor.addGestureRecognizer(tap)
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
+        hideNavigationBar(animated)
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        exposeNavigationBar(animated)
+    }
+    
+    
+    @IBAction func buttonCameraTapped(_ sender: Any) {
+        
+    }
+    
+}
+
+extension HomeViewController {
+    /**
+     Hides navigation bar before the view controller is presented.
+     */
+    func hideNavigationBar(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    /**
+     Exposes navigation bar back after the view controller is dismissed.
+     */
+    func exposeNavigationBar(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
     @objc func toPostVC() {
         performSegue(withIdentifier: "toPostVC", sender: nil)
     }
+}
+
+// Table View Protocol methods
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
@@ -47,11 +75,6 @@ class HomeViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         let cell = UITableViewCell()
         cell.textLabel?.text = "Furkan"
         return cell
-    }
-    
-    
-    @IBAction func buttonCameraTapped(_ sender: Any) {
-        
     }
     
 }

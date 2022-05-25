a279d94a70aec92b1a06707b9bd489b6980e31de
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-25 18:11:13 +0300

    Create PostCollectionViewCell

---
+++ CheckRooMate/View/PostCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PostCollectionViewCell.swift
+//  CheckRooMate
+//
+//  Created by Furkan Açıkgöz on 25.05.2022.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}

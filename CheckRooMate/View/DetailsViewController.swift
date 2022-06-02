5aac4361ab0fb3b91272933bf17f984446d975d4
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-02 14:01:15 +0300

    Add setupUI method to DetailsVC

--- CheckRooMate/View/DetailsViewController.swift
+++ CheckRooMate/View/DetailsViewController.swift
@@ -15,9 +15,13 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var commentTextField: UITextField!
     
+    var post: Post? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Calling setupUI method to initialize UI elements
+        setupUI()
     }
     
     @IBAction func messageBttnTapped(_ sender: Any) {
@@ -27,3 +31,27 @@ class DetailsViewController: UIViewController {
     
 }
 
+extension DetailsViewController {
+    
+    func setupUI() {
+        if let post = post {
+            // Setting username by using userId
+            setUsername()
+            
+            // Setting rest of the proporties with post
+            postTextView.text = post.postContent
+        }
+    }
+    
+    func setUsername() {
+        WebService.run.getUsername(post!.userId!, { usernameArray in
+            if let usernameArray = usernameArray,
+               let usernameObject = usernameArray.first,
+               let username = usernameObject.username{
+                DispatchQueue.main.async {
+                    self.labelUsername.text = username
+                }
+            }
+        })
+    }
+}

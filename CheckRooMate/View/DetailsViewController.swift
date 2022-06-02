46cb6a5085cb6a985ce5afb7db756a3a7a7fe4b4
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-02 16:24:46 +0300

    Add fetchPostComments method into DetailsVC and table view delegation methods

--- CheckRooMate/View/DetailsViewController.swift
+++ CheckRooMate/View/DetailsViewController.swift
@@ -17,10 +17,15 @@ class DetailsViewController: UIViewController {
     
     var post: Post? = nil
     
+    var postComments = [PostComment]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Calling setupUI method to initialize UI elements
+        // Conforming to table view delegate.
+        tableView.dataSource = self
+        
+        // Calling setupUI method to initialize UI elements.
         setupUI()
     }
     
@@ -35,11 +40,21 @@ extension DetailsViewController {
     
     func setupUI() {
         if let post = post {
-            // Setting username by using userId
+            // Setting username by using userId.
             setUsername()
             
-            // Setting rest of the proporties with post
+            // Setting rest of the proporties with post.
             postTextView.text = post.postContent
+            
+            // Fetch comments and assign them to postComments.
+            fetchPostComments(post) { [unowned self] comments in
+                if let comments = comments {
+                    self.postComments = comments
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            }
         }
     }
     
@@ -53,5 +68,36 @@ extension DetailsViewController {
                 }
             }
         })
+    }
+    
+    /**
+     Fetch comments through WebService getPostComments method and
+     then assign them to postComments array in addition to returning
+     back with Completion Handler object.
+     
+     - parameter post: Specific post which is used to get postId in it.
+     */
+    func fetchPostComments(_ post: Post,
+                           _ completionHandler: @escaping ([PostComment]?)->(Void) ){
+        
+        WebService.run.getPostComments(post.postId!) { comments in
+            if let comments = comments {
+                self.postComments = comments
+                completionHandler(comments)
+            }
+        }
+    }
+}
+
+extension DetailsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postComments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = postComments[indexPath.row].commentContent
+        return cell
     }
 }

18f31c0089151a6ff1db72fc609a08f46cf3c7db
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-07 20:31:23 +0300

    Create VisitedProfileViewController and connect its UI elements to its VC

---
+++ CheckRooMate/View/VisitedProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  VisitedProfileViewController.swift
+//  CheckRooMate
+//
+//  Created by Furkan Açıkgöz on 7.06.2022.
+//
+
+import UIKit
+
+class VisitedProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var labelProfile: UILabel!
+    @IBOutlet weak var labelUsername: UILabel!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelSurname: UILabel!
+    @IBOutlet weak var labelEmail: UILabel!
+    @IBOutlet weak var labelPhone: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addBttnTapped(_ sender: Any) {
+    }
+    
+
+}
